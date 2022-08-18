b22bd39747f020c1825725110ee51c3538f5e025
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-18 16:39:52 +0600

    TO DO Project

---
+++ todo.js
@@ -0,0 +1,25 @@
+let count = 0;
+document.getElementById('input-btn').addEventListener('click', function () {
+    const inputValue = document.getElementById('input-value').value;
+    count++;
+
+    const mainContainer = document.getElementById('content-container');
+    const tableContainer = document.createElement('tr');
+    tableContainer.innerHTML = `
+    <th scope="row">${count}</th>
+    <td>${inputValue}</td>
+    <td>
+    <button class="btn btn-primary delete-btn">Delete</button>
+    <button class="btn btn-secondary">Edit</button>
+    </td>
+    `
+    mainContainer.appendChild(tableContainer);
+    document.getElementById('input-value').value = '';
+
+    const deleteBtn = document.getElementsByClassName('delete-btn');
+    for (let button of deleteBtn) {
+        button.addEventListener('click', function (event) {
+            event.target.parentNode.parentNode.style.display = 'none';
+        })
+    }
+})
